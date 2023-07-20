cbb70c13716716051989dd47d0d79c038fd4f331
Sunidhi <sunidhijain0403@gmail.com>
2023-07-20 12:19:54 +0530

    initial commit

---
+++ code/script.js
@@ -0,0 +1,52 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '32280bd2b3msh2922b8bc26522d0p17d554jsn6c597d45ee7e',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+
+
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+
+    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` + city, options)
+        .then(response => response.json())
+        .then((response) => {
+
+            console.log(response)
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+
+
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+  
+
+}
+getWeather("Delhi");   // default weather of Delhi city
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+})
+
+
+
+
+
+
+
+
