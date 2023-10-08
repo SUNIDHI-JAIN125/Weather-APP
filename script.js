8934a20768f360129691d5199da56ee51d5df580
Sunidhi <sunidhijain0403@gmail.com>
2023-10-08 14:35:27 +0530

    responsiveness added

--- script.js
+++ script.js
@@ -7,37 +7,51 @@ const options = {
 };
 
 
-
-
-const getWeather = (city) => {
+  const getWeather = async(city) => {
     cityName.innerHTML = city
+try{
+  let response= await fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` + city, options);
 
-    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` + city, options)
-        .then(response => response.json())
-        .then((response) => {
-
-
-            console.log(response)
-            cloud_pct.innerHTML = response.cloud_pct
-            temp.innerHTML = response.temp
-            feels_like.innerHTML = response.feels_like
-            humidity.innerHTML = response.humidity
-            min_temp.innerHTML = response.min_temp
-            max_temp.innerHTML = response.max_temp
-            wind_speed.innerHTML = response.wind_speed
-            wind_degrees.innerHTML = response.wind_degrees
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
-
-
-        })
-        .catch(err => console.error(err)); 
-}
+     let data = await response.json();
+     if(response.status === 400)
+     {   
+         cloud_pct.innerHTML = "0"
+     temp.innerHTML = "0"
+     feels_like.innerHTML = "0"
+     humidity.innerHTML = "0"
+     min_temp.innerHTML = "0"
+     max_temp.innerHTML = "0"
+     wind_speed.innerHTML = "0"
+     wind_degrees.innerHTML = "0"
+     sunrise.innerHTML = "0"
+     sunset.innerHTML = "0" 
+       return ;
+     }
+        
+            console.log(data);
+            cloud_pct.innerHTML = data.cloud_pct
+            temp.innerHTML = data.temp
+            feels_like.innerHTML = data.feels_like
+            humidity.innerHTML = data.humidity
+            min_temp.innerHTML = data.min_temp
+            max_temp.innerHTML = data.max_temp
+            wind_speed.innerHTML = data.wind_speed
+            wind_degrees.innerHTML = data.wind_degrees
+            sunrise.innerHTML = data.sunrise
+            sunset.innerHTML = data.sunset
+        } catch(error) {
+          console.error('Error: ', error);
+    }
+};
 
 
 submit.addEventListener("click",(e)=>{
-    e.preventDefault()
-    getWeather(city.value)
+    e.preventDefault();
+    if (city.value.trim()) {
+        getWeather(city.value);
+      } else {
+        getWeather("Delhi");
+      }
 })
 
 
